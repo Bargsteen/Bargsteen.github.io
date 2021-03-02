1e3370d35f1aac9dd3a787d8eddb8ca95721acfd
Kasper Dissing Bargsteen <kasper@bargsteen.com>
2021-03-02 19:54:21 +0100

    Set up basic eleventy site with tailwind (postCSS)

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'sans': ['"Helvetica Neue"', 'Helvetica', 'ui-sans-serif', 'system-ui'],
+      'mono': ['ui-monospace', 'SFMono-Regular']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
