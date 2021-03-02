e7bdc9b9f6859fa4de605ac9d1a6dca3a82069c3
Kasper Dissing Bargsteen <kb@concordium.com>
2021-03-02 20:30:35 +0100

    List latest posts on frontpage

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,18 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget("./pcss/tailwind.pcss");
   // eleventyConfig.setWatchThrottleWaitTime(5000); // in milliseconds
+
+  eleventyConfig.addFilter("min", (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if( n < 0 ) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
 }
