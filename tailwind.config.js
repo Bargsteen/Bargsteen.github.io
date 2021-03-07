7bdc99109c4459f0c98bd66f7d2958c3f98d3f03
Kasper Dissing Bargsteen <kb@concordium.com>
2021-03-07 19:25:43 +0100

    Add more mono font variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,17 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'sans': ['"Helvetica Neue"', 'Helvetica', 'ui-sans-serif', 'system-ui'],
-      'mono': ['ui-monospace', 'SFMono-Regular']
+      sans: ['"Helvetica Neue"', 'Helvetica', 'ui-sans-serif', 'system-ui'],
+      mono: [
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
     },
     extend: {},
   },
