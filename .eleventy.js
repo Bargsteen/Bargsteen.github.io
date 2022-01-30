d584c48a253a1b43848a92848751704368ffc434
Kasper Dissing Bargsteen <kb@concordium.com>
2022-01-30 17:35:23 +0100

    Show date of post in post lists

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,24 @@ module.exports = function (eleventyConfig) {
     return Math.min.apply(null, numbers);
   });
 
+  eleventyConfig.addFilter("toThreeLetterMonth", n => {
+    switch(n) {
+      case 0: return "Jan";
+      case 1: return "Feb";
+      case 2: return "Mar";
+      case 3: return "Apr";
+      case 4: return "May";
+      case 5: return "Jun";
+      case 6: return "Jul";
+      case 7: return "Aug";
+      case 8: return "Sep";
+      case 9: return "Oct";
+      case 10: return "Nov";
+      case 11: return "Dec";
+      default: return `${n}`;
+    }
+  });
+
   // Get `n` elements of a collection. Use negative `n` to take from the end.
   eleventyConfig.addFilter("take", (array, n) => {
     if (n < 0) {
